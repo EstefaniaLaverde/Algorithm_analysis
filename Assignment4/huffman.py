0b7160fc8c48946770bc8e410d0d11b1aaafce01
PabloOrtegaC <78177335+PabloOrtegaC@users.noreply.github.com>
2025-02-26 15:23:27 -0500

    Cambios finales A4

--- Assignment4/huffman.py
+++ Assignment4/huffman.py
@@ -1,6 +1,44 @@
 import heapq
+import math
+import sys
+
+
+def print_codification_report(text,codification):
+    #hacer prints con reportes de la codificacion
+    #la codificacion
+    #numero de bits esperado
+    #entropia en el peor de los casos
+    #numero total de bits para el texto comprimodo
+
+    counts = {}
+    for char in text:
+        if char not in counts:
+            counts[char] = 0
+        counts[char] += 1
+
+    print('codification:')
+    print(sorted(list(codification.items())))
+
+    text_len=sum(counts.values())
+
+
+    expected_bit_count=0
+    for c,count in counts.items():
+        expected_bit_count+=(count/text_len)*len(codification[c])
+    print('expected bit count per character:',expected_bit_count)
+
+
+    print('worst case entropy:',math.log2(len(counts)))
+
+
+    total_bits_compr=0
+    for c,count in counts.items():
+        total_bits_compr+=count*len(codification[c])
+
+    print('total bits on compressed file:',total_bits_compr)
 
 class Node:
+    """Se construye el arbol binario usando la cola de prioridad y la clase node. """
     def __init__(self, freq=0, char=None):
         self.char = char
         self.left = None
@@ -47,24 +85,9 @@ def huffman(text):
     return Q[0][1] if Q else None
 
 
-def visualize_tree(root, level=0, prefix="Root: "):
-    if root is None:
-        return
-    
-    indent = "__" * level
-    
-    if root.char is not None:
-        print(f"{indent}{prefix}'{root.char}' (freq: {root.freq:.2f})")
-    else:
-        print(f"{indent}{prefix}Node (freq: {root.freq:.2f})")
-    
-    if root.left:
-        visualize_tree(root.left, level + 1, "L: ")
-    if root.right:
-        visualize_tree(root.right, level + 1, "R: ")
-
 
 def get_huffman_codes(root):
+    """ Determinar los codigos de huffman recorriendo el arbol binario resultante y asignando 0 o 1 a los arcos"""
     codes = {}
 
     def traverse(node, current_code=""):
@@ -82,14 +105,29 @@ def get_huffman_codes(root):
     return codes
 
 
-text = "aabbcddddddddd"
-tree = huffman(text)
-# visualizacion
-print("Huffman Tree:")
-visualize_tree(tree)
-print('')
-# codigos
-codes = get_huffman_codes(tree)
-print("Huffman Codes:")
-for char, code in codes.items():
-    print(f"'{char}': {code}")
+
+def encode_text(text, codification):
+    encoded = ""
+    for char in text:
+        if char in codification:
+            encoded += codification[char]
+        else:
+            raise ValueError(f"Character '{char}' not found in codification mapping.")
+    return encoded
+
+
+
+if __name__=='__main__':
+    input_file=sys.argv[1]
+    output_file=sys.argv[2]
+
+    with open(input_file) as f:
+        text=f.read()
+    
+    tree = huffman(text)
+    codification=get_huffman_codes(tree)
+    cod_text = encode_text(text,codification)
+    print_codification_report(text,codification)
+
+    with open(output_file,'w') as f:
+        f.write(cod_text)
