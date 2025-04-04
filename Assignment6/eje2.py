8f743b0f9646a634d758431b6fb564f7a6210ade
estefaniaLaverde26R <estefania.laverde@ressolve.ai>
2025-04-04 13:01:00 -0500

    Add heap

--- Assignment6/eje2.py
+++ Assignment6/eje2.py
@@ -1,12 +1,14 @@
 # Escoger el vértice de mayor grado, descartar los ejes que llegan al vertice escogido y repetir hasta que no queden ejes.
+import heapq
 
-def solve(edges:list) -> list:
+def solve(edges:set) -> list:
 
     sol = []
 
     # Initialize - save degrees and neighbours
     neighbours = {}
     degrees = {}
+    heap = []
 
     for edge in edges:
         v1,v2 = edge
@@ -25,31 +27,38 @@ def solve(edges:list) -> list:
             degrees[v2] = 0
         degrees[v2] += 1
 
+    # organize min heap
+    for vertex, degree in degrees.items():
+        heapq.heappush(heap, (-degree,vertex))
 
     while edges:
-        vertex_max_degree = list(degrees.keys())[list(degrees.values()).index(max(list(degrees.values())))]
+        vertex_max_degree = heapq.heappop(heap)[-1]
 
         # print("Vertex with maximum degree: ", vertex_max_degree)
-        # print("Current degrees of all vertices: ", degrees)
+        # print("Current degrees of all vertices: ", heap)
         # print("Current neighbours of all vertices: ", neighbours)
 
         neighbours_vertex_max_degrees = neighbours[vertex_max_degree]
         for neigh in neighbours_vertex_max_degrees:
 
             # Update degrees and neighbours 
-            degrees[neigh] -= 1
+            heap.remove((-degrees[neigh], neigh))
+            degrees[neigh] += 1
+            heapq.heappush(heap, (-degrees[neigh], neigh))
+
             neighbours[neigh].remove(vertex_max_degree)
 
-            # Pop edge
+            # Remove edge
             edge = tuple(sorted((neigh,vertex_max_degree)))
             edges.remove(edge)
 
         degrees[vertex_max_degree] = -1
+
         neighbours[vertex_max_degree] = set()
         sol.append(vertex_max_degree)
 
     return sol
 
 # TODO: read tests 
-#edges = [(1,2),(2,3),(3,4),(2,4),(2,5)]
-#print(solve(edges))
+# edges = {(1,2),(2,3),(3,4),(2,4),(2,5)}
+# print(solve(edges))
