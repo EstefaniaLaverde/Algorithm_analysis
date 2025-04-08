760ca89971a0bfc4634cb4ab93dca0641a56c555
PabloOrtegaC <78177335+PabloOrtegaC@users.noreply.github.com>
2025-04-08 13:04:37 -0500

    read me y tests

--- Assignment6/eje3.py
+++ Assignment6/eje3.py
@@ -1,60 +1,43 @@
 # Escoger arbitrariamente un eje, incluir el vértice de mayor grado de los dos vértices conectados por el eje, descartar todos los demás ejes conectados por el vértice escogido y repetir hasta que no queden ejes.
 import random
 
-def solve(edges:list) -> list:
+def solve(edges: list) -> list:
     sol = []
-
-    # Initialize - save degrees and neighbours
+    
+    # almacenar los ejes en un set para eliminacion O(1)
+    edge_set = {tuple(sorted(edge)) for edge in edges}
+    
     neighbours = {}
     degrees = {}
-
-    for edge in edges:
-        v1,v2 = edge
-
-        if v1 not in neighbours:
-            neighbours[v1] = set()
-        neighbours[v1].add(v2)
-        if v2 not in neighbours:
-            neighbours[v2] = set()
-        neighbours[v2].add(v1)
-
-        if v1 not in degrees:
-            degrees[v1] = 0
-        degrees[v1] += 1
-        if v2 not in degrees:
-            degrees[v2] = 0
-        degrees[v2] += 1
-
-
-    while edges:
-        edge = random.choice(edges)
-        v1, v2 = edge
-
-        vertex_max_degree = [v1,v2][[degrees[v1], degrees[v2]].index(max([degrees[v1], degrees[v2]]))]
-
-        #print("Edges: ", edges)
-        #print("Selected edge: ", edge)
-        #print("Current degrees of all vertices: ", degrees)
-        #print("Current neighbours of all vertices: ", neighbours)
-        #print("Vertex with maximum degree: ", vertex_max_degree)
-
-        # update neighbours and degrees and delete edge
-        for neighbour in neighbours[vertex_max_degree]:
-
-            edge_neigh = tuple(sorted((vertex_max_degree, neighbour)))
-            edges.remove(edge_neigh)
-
-            neighbours[neighbour].remove(vertex_max_degree)
+    for u, v in edge_set:
+        neighbours.setdefault(u, set()).add(v)
+        neighbours.setdefault(v, set()).add(u)
+        degrees[u] = degrees.get(u, 0) + 1
+        degrees[v] = degrees.get(v, 0) + 1
+    
+    while edge_set:
+        edge = random.choice(tuple(edge_set))
+        u, v = edge
+        
+        if degrees[u] >= degrees[v]:
+            chosen = u
+        else:
+            chosen = v
+        
+        sol.append(chosen)
+        
+        for neighbour in list(neighbours.get(chosen, [])):
+            edge_neigh = tuple(sorted((chosen, neighbour)))
+            edge_set.discard(edge_neigh)
+            
+            neighbours[neighbour].discard(chosen)
             degrees[neighbour] -= 1
-
-        degrees[vertex_max_degree] = -1
-        neighbours[vertex_max_degree] = set()
-
-        sol.append(vertex_max_degree)
-
+        
+        neighbours[chosen].clear()
+        degrees[chosen] = -1
+    
     return sol
 
-# TODO: add main
-
-#edges = [(1,2),(2,3),(3,4),(2,4),(2,5)]
-#print(solve(edges))
+# Ejemplo de uso:
+# edges = [(1, 2), (2, 3), (3, 4), (2, 4), (2, 5)]
+# print(solve(edges))
---
+++ Assignment6/test.py
@@ -0,0 +1,61 @@
+import random
+import time
+import pandas as pd
+from eje1 import solve as solve1
+from eje2_buckets import solve as solve2_buckets
+from eje2 import solve as solve2
+from eje3 import solve as solve3
+from eje4 import solve as solve4
+
+def generar_grafo_aleatorio(num_vertices, num_aristas):
+    # Grafos sin loops o multiejes
+    if num_aristas > (num_vertices * (num_vertices - 1)) // 2:
+        num_aristas = (num_vertices * (num_vertices - 1)) // 2 
+
+    edges = set()
+    while len(edges) < num_aristas:
+        u = random.randint(0, num_vertices - 1)
+        v = random.randint(0, num_vertices - 1)
+        if u != v:
+            e = tuple(sorted([u, v]))
+            edges.add(e)
+    return list(edges)
+
+def probar_algoritmos():
+    tamanos = [100, 1000, 10000]
+    multiplicadores_aristas = [10, 50, 100]
+
+    resultados = []
+
+    algoritmos = [
+        ("Alg.1", solve1),
+        ("Alg.2", solve2_buckets),
+        ("Alg.3", solve3),
+        ("Alg.4", solve4)
+    ]
+    
+    for n in tamanos:
+        for mult in multiplicadores_aristas:
+            m = n * mult
+            edges = generar_grafo_aleatorio(n, m)
+            
+            for alg_n, alg_f in algoritmos:
+
+                start = time.time()
+                cover = alg_f(edges.copy())
+                end = time.time()
+                algoritmos.append({
+                    "Vertices": n,
+                    "Multiplicador": mult,
+                    "Edges": m,
+                    "Algorithm": alg_n,
+                    "Cover Size": len(cover),
+                    "Time (s)": end - start
+                })
+    
+    df = pd.DataFrame(resultados)
+    return df
+
+if __name__ == "__main__":
+    df = probar_algoritmos()
+    print(df.to_markdown(index=False))
