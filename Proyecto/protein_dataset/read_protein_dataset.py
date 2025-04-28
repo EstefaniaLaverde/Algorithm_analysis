2863891b79837ad793c4bf3b8f7ae9512d862619
estefaniaLaverde26R <estefania.laverde@ressolve.ai>
2025-04-28 17:19:14 -0500

    Añado dataset de proteinas

---
+++ Proyecto/protein_dataset/read_protein_dataset.py
@@ -0,0 +1,25 @@
+with open('truncated_dataset.txt', 'r') as file:
+    lines = file.readlines()
+
+graphs = []
+edges = []
+labels = {}
+for line in lines:
+    if line.startswith('v'):
+        _, node, label = line.split()
+        node = int(node)
+        if node not in labels:
+            labels[node] = int(label)
+
+    elif line.startswith('e'):
+        _, node1, node2, _ = line.split()
+        edges.append((int(node1), int(node2)))
+
+    else:
+        graphs.append([edges, labels])
+        edges = []
+        labels = {}
+
+print("Graphs loaded:", len(graphs))
+print("First graph:", graphs[1])
+        
